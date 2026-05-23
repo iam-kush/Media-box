5e7a91cc6d2045ad8fd39c8b2ecdb19cca51c09b
kushal <kushalts2005@gmail.com>
2026-05-23 18:21:09 +0530

    token generations

--- src/index.js
+++ src/index.js
@@ -11,3 +11,12 @@ dotenv.config({
 
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT||8000,()=>{
+        console.log(`server is running on port ${process.env.PORT}`);
+    })
+})
+.catch((err)=>{
+    console.log("mongobd connection fail",err);
+    
+})
